c293fb23a592621866b8ed149bf965d0d8e9c644
Nika444445 <aster708@yandex.ru>
2026-03-04 13:53:18 +0300

    s

--- js/jss.js
+++ js/jss.js
@@ -5,3 +5,18 @@ b = [10,11,12,13,14]
 
 ab = b.map((b, i) => b + a[i]); 
 console.log(ab)
+
+
+// 2
+
+let d = [1, 2, 8, "+", 3, 4, "-", 7, "+", 5];
+let s = d.splice(1, 7); 
+let pyst = []; 
+
+for (let i = 0; i < s.length; i++) {
+  if (typeof s[i] === 'number') {
+    res.push(s[i]);
+  } 
+}
+
+console.log(pyst);
