298e4035d49465c1968e53ec7c0607659df333a3
Nika444445 <aster708@yandex.ru>
2026-03-31 05:54:50 +0300

    место и тд

--- js/jss.js
+++ js/jss.js
@@ -1,37 +1,45 @@
 
-// 1 
-a = [1, 2, 3, 4, 5]
-b = [10,11,12,13,14]
+// // 1 
+// a = [1, 2, 3, 4, 5]
+// b = [10,11,12,13,14]
 
-ab = b.map((b, i) => b + a[i]); 
-console.log(ab)
+// ab = b.map((b, i) => b + a[i]); 
+// console.log(ab)
 
-// 2
+// // 2
 
-c = ["A", 1, [2,3],[[4]],"5","B"].flat(5)
+// c = ["A", 1, [2,3],[[4]],"5","B"].flat(5)
 
-c.shift()
-c.pop()
-c = c.map(Number);
-console.log(c)
+// c.shift()
+// c.pop()
+// c = c.map(Number);
+// console.log(c)
 
-// 3
+// // 3
 
-let d = [1, 2, 8, "+", 3, 4, "-", 7, "+", 5];
-let s = d.splice(1, 7); 
-let pyst = []; 
+// let d = [1, 2, 8, "+", 3, 4, "-", 7, "+", 5];
+// let s = d.splice(1, 7); 
+// let pyst = []; 
 
-for (let i = 0; i < s.length; i++) {
-  if (typeof s[i] === 'number') {
-    pyst.push(s[i]);
-  } 
-}
-console.log(pyst);
-// 4
+// for (let i = 0; i < s.length; i++) {
+//   if (typeof s[i] === 'number') {
+//     pyst.push(s[i]);
+//   } 
+// }
+// console.log(pyst);
+// // 4
 
 
-e = [[0]],[[1]],[[[[2]]]]
+// e = [[0]],[[1]],[[[[2]]]]
 
-// 5
+// // 5
 
-a = [1,2,3,4,5]
+// a = [1,2,3,4,5]
+
+
+document.getElementById('mapCont').innerHTML = `
+    <a href="https://yandex.ru/maps/?text=Москва, Большая садовая, 15" target="_blank" class="map_link">
+        <img src="https://static-maps.yandex.ru/1.x/?ll=37.592,55.765&z=16&size=600,450&l=map&pt=37.592,55.765,pm2rdm" 
+             alt="Карта проезда" class="map_image">
+    </a>
+`;
